924f157377715821444f8771409ce0286a3f47d4
Markfady <markfady9@gmail.com>
2024-08-08 12:52:17 +0300

    register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  registerForm= this.formbuilder.group({
+    fullName: ['', [Validators.required, Validators.minLength(8)]],
+      email:['',[Validators.required,Validators.email]],
+      password:['',Validators.required],
+      confirmPassword:['',Validators.required]
+    }, {
+      validators: this.passwordMatchValidator.bind(this) // Bind the method correctly (without bind it used before declaration solve this problem)
+    });
+  constructor(private formbuilder:FormBuilder){}
+
+  get fullName(){
+    return this.registerForm.controls['fullName']
+  }
+  get email(){
+    return this.registerForm.controls['email']
+  }
+  get password(){
+    return this.registerForm.controls['password']
+  }
+  get confirmPassword(){
+    return this.registerForm.controls['confirmPassword']
+  }
+   // Custom Validator Method
+   passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+    if (!password || !confirmPassword) {
+      return null;
+    }
+    return password.value === confirmPassword.value ? null : { passwordMismatch: true };
+  }
 }
