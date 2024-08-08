a778fd38be187e3643885b820b3662be50e4c952
Markfady <markfady9@gmail.com>
2024-08-08 18:01:32 +0300

    toggle report function

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ViewChild } from '@angular/core';
 
+import { Sidebar } from 'primeng/sidebar';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,6 +10,16 @@ import { Router } from '@angular/router';
 })
 
 export class HomeComponent {
+  sidebarVisible: boolean = true; // Keep the sidebar open by default
+  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+  isReportsOpen: boolean = false;
+  toggleReports() {
+    this.isReportsOpen = !this.isReportsOpen;
+  }
+
+  closeCallback(event: Event) {
+    this.sidebarVisible = false;
+  }
 
 constructor(private route:Router){}
   logout(){
