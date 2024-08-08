f7b4e2161b0c702fd7934883b5e6d2a02ef32593
Markfady <markfady9@gmail.com>
2024-08-08 12:52:46 +0300

    edit wrong page path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
     path: 'home',
     component: HomeComponent
   },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
